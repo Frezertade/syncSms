554619ee64ce3d8033f559643f06cacbaccb8500
bengeos <george.beng@gmail.com>
2016-03-02 10:26:36 -0800

    update the new syncdevice and usercomrole models

--- module/SMS_API/src/SMS_API/Model/SyncDevice.php
+++ module/SMS_API/src/SMS_API/Model/SyncDevice.php
@@ -11,5 +11,57 @@ namespace SMS_API\Model;
 
 class SyncDevice
 {
+    protected $DeviceID;
+    protected $SecretNo;
+    protected $CompanyID;
+
+    /**
+     * @return mixed
+     */
+    public function getDeviceID()
+    {
+        return $this->DeviceID;
+    }
+
+    /**
+     * @param mixed $DeviceID
+     */
+    public function setDeviceID($DeviceID)
+    {
+        $this->DeviceID = $DeviceID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecretNo()
+    {
+        return $this->SecretNo;
+    }
+
+    /**
+     * @param mixed $SecretNo
+     */
+    public function setSecretNo($SecretNo)
+    {
+        $this->SecretNo = $SecretNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompanyID()
+    {
+        return $this->CompanyID;
+    }
+
+    /**
+     * @param mixed $CompanyID
+     */
+    public function setCompanyID($CompanyID)
+    {
+        $this->CompanyID = $CompanyID;
+    }
+
 
 }
--- module/SMS_API/src/SMS_API/Model/UserComRole.php
+++ module/SMS_API/src/SMS_API/Model/UserComRole.php
@@ -11,5 +11,118 @@ namespace SMS_API\Model;
 
 class UserComRole
 {
+    protected $id;
+    protected $User_ID;
+    protected $Company_ID;
+    protected $Role;
+    protected $created;
+    protected $updated;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserID()
+    {
+        return $this->User_ID;
+    }
+
+    /**
+     * @param mixed $User_ID
+     */
+    public function setUserID($User_ID)
+    {
+        $this->User_ID = $User_ID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompanyID()
+    {
+        return $this->Company_ID;
+    }
+
+    /**
+     * @param mixed $Company_ID
+     */
+    public function setCompanyID($Company_ID)
+    {
+        $this->Company_ID = $Company_ID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->Role;
+    }
+
+    /**
+     * @param mixed $Role
+     */
+    public function setRole($Role)
+    {
+        $this->Role = $Role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param mixed $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * @param mixed $updated
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    public function getArray(){
+        return array(
+            'id' => $this->getId(),
+            'user_id' => $this->getUserID(),
+            'company_id' => $this->getCompanyID(),
+            'role' => $this->getRole(),
+            'created' => $this->getCreated(),
+            'updated' => $this->getUpdated(),
+        );
+    }
 
 }

6f7de2dc9a33d467d1472897cdd344385f8298f9
bengeos <george.beng@gmail.com>
2016-03-11 09:48:49 -0800

    add new interfaces for smsService class

--- module/SMS_API/src/SMS_API/Service/smsService.php
+++ module/SMS_API/src/SMS_API/Service/smsService.php
@@ -10,27 +10,50 @@ namespace SMS_API\Service;
 
 
 use SMS_API\Model\IncomingSMS;
+use SMS_API\Model\OutgoingSMS;
 use SMS_API\Model\SyncDevice;
 use SMS_API\Model\User;
 
 interface smsService
 {
-    public function saveIncoming(IncomingSMS $sms);
-    public function getOutgoingSMS($device_id);
+
+
     public function isValidUser(User $user);
     public function isValidDevice(SyncDevice $device);
     public function getAuthenticationService();
     public function authenticate($userName,$userPass);
     public function addUser(User $user);
-    public function getAllIncoming(User $user);
-    public function getAllOutgoing(User $user);
+
+
     public function getComRole(User $user);
 
+
+    /**
+     * Service related to Incoming SMS
+     */
+    public function saveIncoming(IncomingSMS $sms);
     public function saveIncomingLog(User $user,IncomingSMS $sms);
+
+    public function getNewIncoming(User $user);
+    public function getAllIncoming(User $user);
+
+    public function deleteIncoming(User $user, IncomingSMS $sms);
+    public function deleteIncomingLog(User $user, IncomingSMS $sms);
+    public function deleteAllIncoming(User $user);
+    public function deleteAllIncomingLog(User $user);
+
+    /**
+     * Service related to Outgoing SMS
+     */
+    public function getOutgoingSMS($device_id);
+    public function getAllOutgoing(User $user);
+    public function getNewOutgoing(User $user);
+
     public function saveOutgoing(OutgoingSMS $sms);
     public function saveOutgoingLog(User $user,OutgoingSMS $sms);
-    public function getNewIncoming(User $user);
-    public function getNewOutgoing(User $user);
-    public function saveSMSLog(User $user);
 
+    public function deleteOutgoing(User $user, OutgoingSMS $sms);
+    public function deleteOutgoingLog(User $user, OutgoingSMS $sms);
+    public function deleteAllOutgoing(User $user);
+    public function deleteAllOutgoingLog(User $user);
 }

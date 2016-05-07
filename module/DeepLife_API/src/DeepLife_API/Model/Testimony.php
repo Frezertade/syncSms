d4d0f5b45398985511eaa42a939e84cafb4e7d3f
bengeos <george.beng@gmail.com>
2016-05-06 19:16:31 -0700

    add new Testimony model

---
+++ module/DeepLife_API/src/DeepLife_API/Model/Testimony.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roger
+ * Date: 4/16/2016
+ * Time: 11:19 PM
+ */
+
+namespace DeepLife_API\Model;
+
+
+class Testimony
+{
+    protected $id;
+    protected $user_id;
+    protected $title;
+    protected $detail;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetail()
+    {
+        return $this->detail;
+    }
+
+    /**
+     * @param mixed $detail
+     */
+    public function setDetail($detail)
+    {
+        $this->detail = $detail;
+    }
+
+}
